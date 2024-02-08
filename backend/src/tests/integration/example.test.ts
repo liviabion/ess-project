395e65ead2dba2f7f53cbf1773d934ec97de2854
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-02-08 09:19:20 -0300

    fix(backendTestDatabase): fix value clearance after testing

--- backend/src/tests/integration/example.test.ts
+++ backend/src/tests/integration/example.test.ts
@@ -6,14 +6,23 @@ import request from 'supertest';
 import app from '../../app';
 
 const feature = loadFeature('../backend/src/tests/features/example.feature');
+const prismaTestClient = new PrismaClient();
+let apiResponse: request.Response;
 
 defineFeature(feature, test => {
+
+  beforeAll(async () => {
+    await database.clearValues();
+    await database.connect();
+  });
+
+  afterAll(async () => {
+    await database.clearValues();
+    await database.disconnect();
+  });
   
-  test('Cenário de exemplo', ({ given, when, then, and }) => {
-    const prismaTestClient = new PrismaClient();
-    
+  test('Cenário de exemplo', ({ given, when, then, and }) => {  
     given(/^I don't have any user with email "(.*)"$/, async (email) => {
-      await database.connect();
       const userExists = await prismaTestClient.user.findFirst({
         where: {
           email
@@ -26,44 +35,18 @@ defineFeature(feature, test => {
     when(/^I insert a user with: name "(.*)", email "(.*)", cpf "(.*)", password "(.*)", phone "(.*)"$/, async (
       name, email, cpf, password, phone
     ) => {
-      const newUser = await prismaTestClient.user.create({
-        data: {
-          name,
-          email,
-          cpf,
-          password,
-          phone
-        }
-      })
-
-      expect(newUser).toHaveProperty('name', name);
-      expect(newUser).toHaveProperty('email', email);
-      expect(newUser).toHaveProperty('cpf', cpf);
-      expect(newUser).toHaveProperty('password', password);
-      expect(newUser).toHaveProperty('phone', phone);
+      apiResponse = await request(app).post('/user').send({
+        name, email, cpf, password, phone
+      });
+      expect(apiResponse.status).toBe(201);
     });
 
     then(/^I should have a user with email "(.*)"$/, async (email) => {
-      const user = await prismaTestClient.user.findFirst({
-        where: {
-          email,
-        }
-      });
-
-      expect(user).toHaveProperty('email', email);
+      expect(apiResponse.body.data).toHaveProperty('email', email);
     });
 
     and(/^I should have this user with id "(.*)"$/, async (id) => {
-      const user = await prismaTestClient.user.findFirst({
-        where: {
-          id: Number(id),
-        }
-      });
-
-      expect(user).toHaveProperty('id', Number(id));
-
-      await database.clearValues();
-      await database.disconnect();
+      expect(apiResponse.body.data).toHaveProperty('id', Number(id));
     });
 
   });
