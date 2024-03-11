d004912cf0729bc6cc3dce226dea298047fd4e25
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2024-03-10 21:29:26 -0300

    feat(metodos_de_pagamento): add Test (scenario: card registered with sucess) working

--- frontend/tests/features/cardPayment.feature
+++ frontend/tests/features/cardPayment.feature
@@ -4,40 +4,25 @@ Feature: Cadastro e manuntenção de métodos de pagamento
     So that eu possa confirmar o pagamento dos produtos
 
 Scenario: Cadastro de cartão de crédito com sucesso
-Given o usuário está na página "./newCardPayment"
-Given não existe cartão de crédito cadastrado com número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha "13456" e tipo "credit"
-When ele clica em "+"
-When digita nos campos número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha "13456" e seleciona "Credit"
-When o usuário clica em "Cadastrar" e confirma clicando em "Sim"
-Then ele visualiza a mensagem "Cartão cadastrado com sucesso"
-Then ele é redirecionado para a página "./cardPayment"
-Then o cartão "1234 5678 9101 1121" aparece na lista de cartões disponíveis
+    Given o usuário está na página "http://localhost:3000/newCardPayment"
+    When preenche os campos card_number "1234 5678 9101 1121", name "José", cvv "999", expire_date "2/25", password "13456", type "Credit" e aperta o botão Cadastrar
+    When o usuário confirma clicando em "Sim"
+    Then ele visualiza a mensagem "Cartão cadastrado com sucesso"
+    Then ele é redirecionado para a página "http://localhost:3000/cardPayment"
+    Then o cartão de número "1234 5678 9101 1121" e tipo "Credit" aparece na lista de cartões disponíveis
 
-Scenario: Cadastro de cartão de crédito repetido
-Given o usuário "José" está logado como cliente
-Given está na página "./newCardPayment"
-Given existe cartão de crédito cadastrado com número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha "13456" e tipo "credit"
-When ele clica em "+"
-When digita nos campos número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha "13456" e seleciona "Credit"
-When o usuário "José" clica em "Cadastrar" e confirma clicando em "Sim"
-Then ele visualiza a mensagem "Esse cartão já está cadastrado"
-Then ele permanece na página "./newCardPayment"
+# Scenario: Cadastro de cartão de crédito incompleto
+#     Given o usuário  está na página "./newCardPayment"
+#     When preenche os campos card_number "1234 5678 9101 1121", name "José", cvv "999", expire_date "2/25", password "", type "Credit" e aperta o botão Cadastrar
+#     When o usuário confirma clicando em "Sim"
+#     Then ele visualiza a mensagem "Preencha todos os campos antes de adicionar."
+#     Then ele permanece na página "./newCardPayment"
 
-Scenario: Cadastro de cartão de crédito incompleto
-Given o usuário "José" está logado como cliente
-Given está na página "./newCardPayment"
-Given existe cartão de crédito cadastrado com número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha "13456" e tipo "credit"
-When ele clica em "+"
-When digita nos campos número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25", senha em branco e seleciona "Credit"
-When o usuário "José" clica em "Cadastrar" e confirma clicando em "Sim"
-Then ele visualiza a mensagem "Preencha todos os campos antes de adicionar."
-Then ele permanece na página "./newCardPayment"
-
-Scenario: Remoção de um método de pagamento
-Given o usuário "José" está logado como cliente
-Given está na tela "./cardPayment"
-Given existe cartão de crédito cadastrado com número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25" e senha "13456" 
-When ele seleciona o cartão de número "1234 5678 9101 1121" e tipo "credit"
-When ele seleciona a opção "Remover cartão" e confirma clicando em "Sim"
-Then ele vê uma mensagem "Cartão removido com sucesso!"
-And o cartão "1234 5678 9101 1121" é removido da lista de cartões disponíveis
+# Scenario: Remoção de um método de pagamento
+# Given o usuário "José" está logado como cliente
+# Given está na tela "./cardPayment"
+# Given existe cartão de crédito cadastrado com número "1234 5678 9101 1121", nome de titular "José", CVV "999", data de validade "2/25" e senha "13456" 
+# When ele seleciona o cartão de número "1234 5678 9101 1121" e tipo "credit"
+# When ele seleciona a opção "Remover cartão" e confirma clicando em "Sim"
+# Then ele vê uma mensagem "Cartão removido com sucesso!"
+# And o cartão "1234 5678 9101 1121" é removido da lista de cartões disponíveis
--- frontend/tests/step-definition/cardPayment.step.ts
+++ frontend/tests/step-definition/cardPayment.step.ts
@@ -3,24 +3,100 @@ import { expect } from '@playwright/test';
 import { ICustomWorld } from '../support/custom-world';
 
 // Scenario: Cadastro de cartão de crédito com sucesso
-Given('o usuário está na página "./newCardPayment"', async function (this: ICustomWorld) {
-    await this.page!.goto('https://playwright.dev/');
+Given('o usuário está na página {string}', async function (this: ICustomWorld, path: string) {
+    await this.page!.goto(path);
 });
 
-When('I open the page', async function (this: ICustomWorld) {
-    // This step is already covered by the 'Given' step above
+Given('o cartão de número {string} e tipo {string} aparece na lista de cartões disponíveis', async function (this: ICustomWorld, card_number: string, type: string) {
+    const element = await expect(this.page?.getByText(card_number)).toBeTruthy();
+    const element2 = await expect(this.page?.getByText(type)).toBeTruthy();
 });
 
-Then('the title has the text "Playwright"', async function (this: ICustomWorld) {
-    await expect(this.page!).toHaveTitle(/Playwright/);
+When('preenche os campos card_number {string}, name {string}, cvv {string}, expire_date {string}, password {string}, type {string} e aperta o botão Cadastrar',
+      async function (
+          this: ICustomWorld,
+          card_number: string,
+          name: string,
+          cvv: string,
+          expire_date: string,
+          password: string,
+          type: string,
+      ) {
+          await this.page!.getByPlaceholder('Número do cartão').fill(card_number)
+          await this.page!.getByPlaceholder('Nome').fill(name);
+          await this.page!.getByPlaceholder('CVV').fill(cvv);
+          await this.page!.getByPlaceholder('Data de validade').fill(expire_date);
+          await this.page!.getByPlaceholder('Senha').fill(password);
+          await this.page!.getByLabel(type).check();
+          await this.page!.locator('button:text("Cadastrar")').click();
+      }
+  );
+
+When('o usuário confirma clicando em {string}', async function (this: ICustomWorld) {
+    await this.page!.locator('button:text("Sim")').click();
 });
 
-// Scenario: Get Started Link
+Then('ele visualiza a mensagem {string}', async function (this: ICustomWorld, alert: string) {
+    this.page?.on("dialog", async (alert) => {
+        expect(alert.type()).toContain("alert")
+        expect(alert.message).toContain("Cartão cadastrado com sucesso")
 
-When('I click at "Get Started"', async function (this: ICustomWorld) {
-    await this.page!.click('text=Get started');
+        await alert.accept()
+    });
 });
 
-Then('the URL has the text "intro"', async function (this: ICustomWorld) {
-    await expect(this.page!).toHaveURL(/.*asdsd/);
+Then('ele é redirecionado para a página {string}', async function (this: ICustomWorld, path: string) {
+    await this.page!.goto(path);
 });
+
+Then('o cartão de número {string} aparece na lista de cartões disponíveis', async function (this: ICustomWorld, card_number: string, type: string) {
+    const element = await expect(this.page?.getByText(card_number)).toBeTruthy();
+    const element2 = await expect(this.page?.getByText(type)).toBeTruthy();
+});
+
+// Scenario: Cadastro de cartão de crédito incompleto
+// Given('o usuário está na página {string}', async function (this: ICustomWorld, path: string) {
+//     await this.page!.goto(path);
+// });
+
+// Given('o cartão de número {string} e tipo {string} aparece na lista de cartões disponíveis', async function (this: ICustomWorld, card_number: string, type: string) {
+//     const element = await expect(this.page?.getByText(card_number)).toBeTruthy();
+//     const element2 = await expect(this.page?.getByText(type)).toBeTruthy();
+// });
+
+// When('preenche os campos card_number {string}, name {string}, cvv {string}, expire_date {string}, password {string}, type {string} e aperta o botão Cadastrar',
+//       async function (
+//           this: ICustomWorld,
+//           card_number: string,
+//           name: string,
+//           cvv: string,
+//           expire_date: string,
+//           password: string,
+//           type: string,
+//       ) {
+//           await this.page!.getByPlaceholder('Número do cartão').fill(card_number)
+//           await this.page!.getByPlaceholder('Nome').fill(name);
+//           await this.page!.getByPlaceholder('CVV').fill(cvv);
+//           await this.page!.getByPlaceholder('Data de validade').fill(expire_date);
+//           await this.page!.getByPlaceholder('Senha').fill(password);
+//           await this.page!.getByLabel(type).check();
+//           await this.page!.locator('button:text("Cadastrar")').click();
+//       }
+//   );
+
+// When('o usuário confirma clicando em {string}', async function (this: ICustomWorld) {
+//     await this.page!.locator('button:text(choice)').click();
+// });
+
+// Then('ele visualiza a mensagem {string}', async function (this: ICustomWorld, alert: string) {
+//     this.page?.on("dialog", async (alert) => {
+//         expect(alert.type()).toContain("alert")
+//         expect(alert.message).toContain("Cartão cadastrado com sucesso")
+
+//         await alert.accept()
+//     });
+// });
+
+// Then('o usuário confirma clicando em "Sim"', async function (this: ICustomWorld) {
+//     await this.page!.locator('button:text("Sim")').click();
+// });
